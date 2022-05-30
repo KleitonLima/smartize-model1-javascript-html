0a2119d28c50bd17542808535409b7d4956abf90
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-30 10:12:25 -0300

    Create clas in script, modal edit and notification

--- assets/script.js
+++ assets/script.js
@@ -1,97 +1,97 @@
 const baseUrl = "http://localhost:3001";
 let listaProdutos = [];
 
-const buscarProdutos = async () => {
-  const res = await fetch(`${baseUrl}/produtos/listar-produtos`);
-  const produtos = await res.json();
+class Requisicoes {
+  async buscarProdutos() {
+    const res = await fetch(`${baseUrl}/produtos/listar-produtos`);
+    const produtos = await res.json();
 
-  listaProdutos = produtos;
+    listaProdutos = produtos;
 
-  return produtos;
-};
-
-buscarProdutos();
-
-const buscarProdutoId = async (id) => {
-  const res = await fetch(`${baseUrl}/produtos/listar-produto/${id}`);
-
-  if (res.status === 404) {
-    return false;
+    return produtos;
   }
-  const produto = await res.json();
 
-  return produto;
-};
+  async buscarProdutoId(id) {
+    const res = await fetch(`${baseUrl}/produtos/listar-produto/${id}`);
 
-const criarProduto = async (tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia) => {
-  const produto = {
-    tipo,
-    marca,
-    modelo,
-    descricao,
-    cor,
-    condicao,
-    foto,
-    preco,
-    garantia,
-  };
-  const res = await fetch(`${baseUrl}/produtos/criar-produto`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    mode: "cors",
-    body: JSON.stringify(produto),
-  });
+    if (res.status === 404) {
+      return false;
+    }
+    const produto = await res.json();
 
-  const novoProduto = await res.json();
-
-  console.log(novoProduto);
-  return novoProduto;
-};
-
-const atualizarProduto = async (id, tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia) => {
-  const produto = {
-    tipo,
-    marca,
-    modelo,
-    descricao,
-    cor,
-    condicao,
-    foto,
-    preco,
-    garantia,
-  };
-  const res = await fetch(`${baseUrl}/produtos/atualizar-produto/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    mode: "cors",
-    body: JSON.stringify(produto),
-  });
-
-  const produtoAtualizado = await res.json();
-
-  return produtoAtualizado;
-};
-
-const excluirProduto = async (id) => {
-  const res = await fetch(`${baseUrl}/produtos/deletar-produto/${id}`, {
-    method: "DELETE",
-    mode: "cors",
-  });
-
-  if (res.status === 204) {
-    return true;
-  } else {
-    return false;
+    return produto;
   }
-};
+
+  async criarProduto(tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia) {
+    const produto = {
+      tipo,
+      marca,
+      modelo,
+      descricao,
+      cor,
+      condicao,
+      foto,
+      preco,
+      garantia,
+    };
+    const res = await fetch(`${baseUrl}/produtos/criar-produto`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(produto),
+    });
+
+    const novoProduto = await res.json();
+
+    return novoProduto;
+  }
+
+  async atualizarProduto(id, tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia) {
+    const produto = {
+      tipo,
+      marca,
+      modelo,
+      descricao,
+      cor,
+      condicao,
+      foto,
+      preco,
+      garantia,
+    };
+    const res = await fetch(`${baseUrl}/produtos/atualizar-produto/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(produto),
+    });
+
+    const produtoAtualizado = await res.json();
+
+    return produtoAtualizado;
+  }
+
+  async excluirProduto(id) {
+    const res = await fetch(`${baseUrl}/produtos/deletar-produto/${id}`, {
+      method: "DELETE",
+      mode: "cors",
+    });
+
+    if (res.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+const requisicoes = new Requisicoes();
 
 const imprimirProdutos = async () => {
+  const produtos = await requisicoes.buscarProdutos();
   document.getElementById("produtoLista").innerHTML = "";
-  const produtos = await buscarProdutos();
 
   produtos.forEach((element) => {
     document.getElementById("produtoLista").insertAdjacentHTML(
@@ -104,7 +104,7 @@ const imprimirProdutos = async () => {
           <div class="ProdutoListaItem__modelo">${element.modelo}</div>
           <div class="ProdutoListaItem__preco">R$ ${element.preco.toFixed(2)}</div>
           <div>
-            <button class="botao-editar">EDITAR</button>
+            <button class="botao-editar" onclick="mostrarModalEditar('${element._id}')">EDITAR</button>
             <button class="botao-deletar" onclick="mostrarModalDeletar('${element._id}')">DELETAR</button>
           </div>
          </div>
@@ -132,17 +132,9 @@ const encontrarProdutoId = async () => {
     document.getElementById("produtoEscolhido").appendChild(mensagemErro);
   }
 
-  const produto = await buscarProdutoId(produtoEscolhido._id);
+  const produto = await requisicoes.buscarProdutoId(produtoEscolhido._id);
 
-  if (!produto) {
-    const mensagemErro = document.createElement("p");
-    mensagemErro.id = "mensagemErro";
-    mensagemErro.classList.add("MensagemErro");
-    mensagemErro.innerText = "Nenhum produto encontrado!";
-
-    document.getElementById("produtoEscolhido").appendChild(mensagemErro);
-  } else {
-    document.getElementById("produtoEscolhido").innerHTML = `
+  document.getElementById("produtoEscolhido").innerHTML = `
   <div class="ProdutoListaItem">
     <img class="ProdutoListaItem__foto" src="${produto.foto}" alt="Foto do ${produto.modelo} " />
     <div>
@@ -155,21 +147,31 @@ const encontrarProdutoId = async () => {
       <div class="ProdutoListaItem__garantia">${produto.garantia}</div>
       <div class="ProdutoListaItem__descricao">${produto.descricao}</div>
       <div>
-        <button class="botao-editar" onclick="">EDITAR</button>
+        <button class="botao-editar" onclick="mostrarModalEditar('${produto._id}')">EDITAR</button>
         <button class="botao-deletar" onclick="mostrarModalDeletar('${produto._id}')">DELETAR</button>
       </div>
 
     </div>
   </div>`;
-  }
+};
+
+const esconderModalCadastro = () => {
+  document.getElementById("fundoModalCadastrar").style.display = "none";
+  document.getElementById("inputTipo").value = "";
+  document.getElementById("inputMarca").value = "";
+  document.getElementById("inputModelo").value = "";
+  document.getElementById("inputDescricao").value = "";
+  document.getElementById("inputCor").value = "";
+  document.getElementById("inputCondicao").value = "";
+  document.getElementById("inputFoto").value = "";
+  document.getElementById("inputPreco").value = "";
+  document.getElementById("inputGarantia").value = "";
 };
 
 const mostrarModalCadastro = () => {
   document.getElementById("fundoModalCadastrar").style.display = "flex";
 };
-const esconderModalCadastro = () => {
-  document.getElementById("fundoModalCadastrar").style.display = "none";
-};
+
 const cadastrarProduto = async () => {
   const tipo = document.getElementById("inputTipo").value,
     marca = document.getElementById("inputMarca").value,
@@ -181,7 +183,13 @@ const cadastrarProduto = async () => {
     preco = document.getElementById("inputPreco").value,
     garantia = document.getElementById("inputGarantia").value;
 
-  const produto = await criarProduto(tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia);
+  const produto = await requisicoes.criarProduto(tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia);
+
+  if (produto) {
+    mostrarNotificacao("sucess", "Produto Cadastrado");
+  } else {
+    mostrarNotificacao("error", "Produto não Cadastrado");
+  }
 
   document.getElementById("produtoLista").insertAdjacentHTML(
     "beforeend",
@@ -193,7 +201,7 @@ const cadastrarProduto = async () => {
       <div class="ProdutoListaItem__modelo">${produto.modelo}</div>
       <div class="ProdutoListaItem__preco">R$ ${produto.preco.toFixed(2)}</div>
       <div>
-        <button class="botao-editar">EDITAR</button>
+        <button class="botao-editar" onclick="mostrarModalEditar('${produto._id}')">EDITAR</button>
         <button class="botao-deletar" onclick="mostrarModalDeletar('${produto._id}')">DELETAR</button>
       </div>
 
@@ -203,23 +211,96 @@ const cadastrarProduto = async () => {
   esconderModalCadastro();
 };
 
+const esconderModalEditar = () => {
+  document.getElementById("fundoModalEditar").style.display = "none";
+};
+
+const mostrarModalEditar = (id) => {
+  document.getElementById("inputTipo").value = "";
+  document.getElementById("inputMarca").value = "";
+  document.getElementById("inputModelo").value = "";
+  document.getElementById("inputDescricao").value = "";
+  document.getElementById("inputCor").value = "";
+  document.getElementById("inputCondicao").value = "";
+  document.getElementById("inputFoto").value = "";
+  document.getElementById("inputPreco").value = "";
+  document.getElementById("inputGarantia").value = "";
+
+  document.getElementById("fundoModalEditar").style.display = "flex";
+
+  const produto = listaProdutos.find((element) => element._id === id);
+
+  document.getElementById("inputTipoEdicao").value = produto.tipo;
+  document.getElementById("inputMarcaEdicao").value = produto.marca;
+  document.getElementById("inputModeloEdicao").value = produto.modelo;
+  document.getElementById("inputDescricaoEdicao").value = produto.descricao;
+  document.getElementById("inputCorEdicao").value = produto.cor;
+  document.getElementById("inputCondicaoEdicao").value = produto.condicao;
+  document.getElementById("inputFotoEdicao").value = produto.foto;
+  document.getElementById("inputPrecoEdicao").value = produto.preco;
+  document.getElementById("inputGarantiaEdicao").value = produto.garantia;
+
+  const botaoSalvarEdicao = document.getElementById("botaoSalvarEdicao");
+
+  botaoSalvarEdicao.addEventListener("click", async () => {
+    const tipo = document.getElementById("inputTipoEdicao").value,
+      marca = document.getElementById("inputMarcaEdicao").value,
+      modelo = document.getElementById("inputModeloEdicao").value,
+      descricao = document.getElementById("inputDescricaoEdicao").value,
+      cor = document.getElementById("inputCorEdicao").value,
+      condicao = document.getElementById("inputCondicaoEdicao").value,
+      foto = document.getElementById("inputFotoEdicao").value,
+      preco = document.getElementById("inputPrecoEdicao").value,
+      garantia = document.getElementById("inputGarantiaEdicao").value;
+
+    await requisicoes.atualizarProduto(id, tipo, marca, modelo, descricao, cor, condicao, foto, preco, garantia);
+
+    esconderModalEditar();
+    imprimirProdutos();
+  });
+};
+
+const esconderModalDeletar = () => {
+  document.getElementById("fundoModalDeletar").style.display = "none";
+};
+
 const mostrarModalDeletar = (id) => {
   document.getElementById("fundoModalDeletar").style.display = "flex";
 
-  const botaoConfirmarExlusao = document.getElementById("botaoConfirmarExclusao");
+  const botaoConfirmarExclusao = document.getElementById("botaoConfirmarExclusao");
 
-  botaoConfirmarExlusao.addEventListener("click", () => {
-    const excluir = excluirProduto(id);
+  botaoConfirmarExclusao.addEventListener("click", async () => {
+    const excluir = await requisicoes.excluirProduto(id);
 
     if (excluir) {
-      alert('Produto deletado')
+      mostrarNotificacao("sucess", "Produto deletado");
     } else {
-      alert('Produto não encontrado')
+      mostrarNotificacao("error", "Produto não encontrado");
     }
     esconderModalDeletar();
     imprimirProdutos();
   });
 };
-const esconderModalDeletar = () => {
-  document.getElementById("fundoModalDeletar").style.display = "none";
+
+const esconderNotificacao = () => {
+  document.getElementById("notification").style.display = "none";
+};
+const mostrarNotificacao = (tipo, frase) => {
+  const notificationSpan = document.getElementById("notificationSpan"),
+    notificationP = document.getElementById("notificationP");
+
+  if (tipo === "sucess") {
+    notificationSpan.innerHTML = "V";
+    notificationSpan.classList = "notificacao-span-sucess";
+  } else if (tipo === "error") {
+    notificationSpan.innerHTML = "X";
+    notificationSpan.classList = "notificacao-span-error";
+  }
+
+  notificationP.innerText = frase;
+
+  document.getElementById("notification").style.display = "flex";
+  setTimeout(() => {
+    esconderNotificacao();
+  }, 3000);
 };
